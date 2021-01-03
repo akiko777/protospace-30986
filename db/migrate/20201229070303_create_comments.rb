ffbbc6c4768400b73c0a18685d0a17b0cfbfb4d1
akiko777 <natsu.hi.boshi731rainbowrose@gmail.com>
2021-01-03 23:10:46 +0900

    first commit

---
+++ db/migrate/20201229070303_create_comments.rb
@@ -0,0 +1,10 @@
+class CreateComments < ActiveRecord::Migration[6.0]
+  def change
+    create_table :comments do |t|
+      t.text :text
+      t.references :user_id, foreign_key: true
+      t.references :prototype_id, foreign_key: true
+      t.timestamps
+    end
+  end
+end
